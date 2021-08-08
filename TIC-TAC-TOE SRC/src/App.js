fb53c2f2f9b9c3f394a28f6402fb641e0a9b7352
Taiwo Momoh <taiwoladipupo@gmail.com>
2021-08-07 21:45:29 -0500

    tic-tac-toe

---
+++ TIC-TAC-TOE SRC/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Game from './components/Game';
+
+import Board from './components/Board'
+
+function App () {
+    return (
+
+      <div className="App">
+        <h1>TIC TAC TOE</h1>
+        <Board />
+      </div>
+    );
+}
+
+
+export default App;
---
+++ TIC-TAC-TOE SRC/src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
+import Game from './components/Game'
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Game />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
